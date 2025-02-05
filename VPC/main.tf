39c8fd41841f757e90255d10bf97e390ffb1fdca
Brian Armstrong <brian.armstrong@kc.frb.org>
2025-02-05 10:23:09 -0600

    formatting

--- VPC/main.tf
+++ VPC/main.tf
@@ -7,8 +7,8 @@
 */
 
 resource "aws_vpc" "main" {
-    cidr_block = "10.0.0.0/16"
-    tags = {
-        Name = "phase2-tf"
-    }
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "phase2-tf"
+  }
 }
