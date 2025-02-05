a82d1b303f7618ce33acf73c8ed4dfcc6c1c4d2f
Brian Armstrong <brian.armstrong@kc.frb.org>
2025-02-05 10:51:06 -0600

    rename files

---
+++ Application/variables.tf
@@ -0,0 +1,10 @@
+
+variable "on_prem_ip" {
+  description = "the on-prem IP list"
+  default     = ["10.0.0.0/8", "162.77.0.0/16", "172.16.0.0/12", "3.83.200.219/32"]
+}
+
+variable "my_ip" {
+  description = "the IP address of FRS Workspaces"
+  default = "3.83.200.219/32"
+}
