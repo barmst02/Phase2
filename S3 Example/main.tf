aeaad318a226710c0f924296dd291ab180678b0b
Brian Armstrong <brian.armstrong@kc.frb.org>
2025-02-19 10:31:27 -0600

    simplify file for typing

--- S3 Example/main.tf
+++ S3 Example/main.tf
@@ -1,29 +1,3 @@
 resource "aws_s3_bucket" "s3_backend" {
-  bucket = "tf-<firstname.lastname>"
-
-  tags = {
-    Name = "Backend"
-  }
-}
-
-provider "aws" {
-  //https://registry.terraform.io/browse/providers
-  //Providers are a logical abstraction of an upstream API. They are responsible for understanding API interactions and exposing resources. 
-  region  = "us-gov-west-1"
-  profile = "default"
-}
-
-//This file is static and will be used as-is across all projects. 
-
-terraform {
-  //While these versions aren't restricted in the Launchpad, they are for CFS2
-  //This is documented on "Supported Terraform Versions" in the Cloud Library 
-  required_version = "<=1.9.8"  //of Terraform
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "= 4.19.0"
-    }
-  }
+  bucket = "<firstname.lastname>-tf"
 }
