7989af32e74417b4c8c88f65479aa9289ea6bae2
Brian Armstrong <brian.armstrong@kc.frb.org>
2025-05-15 13:37:31 -0500

    Updated outputs to match documentation

--- Application/output.tf
+++ Application/output.tf
@@ -3,30 +3,38 @@
 //and can expose information for other Terraform configurations to use. 
 //Output values are similar to return values in programming languages.
 
-output "vpc" {
-  value = data.aws_vpc.main.tags
+output "vpc_id" {
+  value = data.aws_vpc.main.id
 }
 
-output "primary_subnet" {
-  value = data.aws_subnet.public-1a.arn
+output "public_1a_subnet_id" {
+  value = data.aws_subnet.public-1a.id
+}
+
+output "public_2a_subnet_id" {
+  value = data.aws_subnet.public-1a.id
 }
 
 #output "primary_subnet_detail" {
 #  value = data.aws_subnet.public-1a
 #}
 
-output "public_subnets" {
-  value = [for s in data.aws_subnet.public : s.id]
-}
+#output "public_subnets" {
+#  value = [for s in data.aws_subnet.public : s.id]
+#}
 
 output "ami" {
   value = data.aws_ami.al2023.name
 }
 
+output "alb_dns" {
+  value = aws_lb.alb.dns_name
+}
 output "ecs_dns" {
   value = aws_instance.webserver.public_dns
 }
 
-output "alb_dns" {
-  value = aws_lb.alb.dns_name
+output "ecs_ip" {
+  value = aws_instance.webserver.public_ip
 }
+
